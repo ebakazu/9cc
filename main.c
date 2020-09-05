098834eb552910d55f4f6ad5a90a26313746bd31
ebakazu <ebara.kazuma@gmail.com>
2020-09-06 00:59:39 +0900

    9cc.c を main.c に移行

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "9cc.h"
+
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        error("引数の個数が正しくありません");
+        return 1;
+    }
+
+    user_input = argv[1];
+    token = tokenize(user_input);
+    Node *node = expr();
+
+    printf(".intel_syntax noprefix\n");
+    printf(".globl main\n");
+    printf("main:\n");
+
+    gen(node);
+
+    printf("  pop rax\n");
+    printf("  ret\n");
+    return 0;
+}

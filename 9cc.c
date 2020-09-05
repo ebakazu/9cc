ec4967e13b8cccc8b9ef40319c1a03fff3254361
ebakazu <ebara.kazuma@gmail.com>
2020-09-05 23:56:10 +0900

    9cc.h を作成

--- 9cc.c
+++ 9cc.c
@@ -4,69 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// トークンの種類
-typedef enum {
-    TK_RESERVED, // 記号
-    TK_NUM, // 整数トークン
-    TK_EOF, // 入力の終わりを示すトークン
-} TokenKind;
-
-typedef struct Token Token;
-struct Token {
-    TokenKind kind; // トークンの型
-    Token *next;    // 次の入力トークン
-    int val;        // kind が TK_NUM の場合の数値
-    char *str;      // トークン文字列
-    int len;        // トークンの長さ
-};
+#include "9cc.h"
 
 Token *token;
-
-// 抽象構文木 (AST) のノードの種類
-typedef enum {
-    ND_ADD, // +
-    ND_SUB, // -
-    ND_MUL, // *
-    ND_DIV, // /
-    ND_EQ, // ==
-    ND_NE, // !=
-    ND_GT, // >
-    ND_LT, // <
-    ND_GE, // >=
-    ND_LE, // <=
-    ND_NUM, // 整数
-} NodeKind;
-
-typedef struct Node Node;
-
-// 抽象構文木のノードの型
-struct Node {
-    NodeKind kind; // ノードの型
-    Node *lhs;     // 左辺のノード
-    Node *rhs;     // 右辺のノード
-    int val;       // ノードの型がND_NUMの場合に使用
-};
-
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
-Node *new_node_num(int val);
-Node *expr();
-Node *equality();
-Node *relational();
-Node *add();
-Node *mul();
-Node *primary();
-Node *unary();
-void error(char *fmt, ...);
-void error_at(char *loc, char *fmt, ...);
-bool consume(char*);
-void expect(char*);
-int expect_number();
-bool at_eof();
-Token *new_token(TokenKind kind, Token*, char*, int len);
-Token *tokenize(char *p);
-void gen(Node *node);
-
 
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
     Node *node = calloc(1, sizeof(Node));

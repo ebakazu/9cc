0052d02a779172080031cfd6764a25ba5efe3f4a
ebakazu <ebara.kazuma@gmail.com>
2020-09-06 00:58:28 +0900

    parse.c の作成

---
+++ parse.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include "9cc.h"
+
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+
+Node *expr() {
+    Node *node = equality();
+    return node;
+}
+
+Node *equality() {
+    Node *node = relational();
+
+    for (;;) {
+        if (consume("=="))
+            node = new_node(ND_EQ, node, relational());
+        else if (consume("!="))
+            node = new_node(ND_NE, node, relational());
+        else
+            return node;
+    }
+}
+
+Node *relational() {
+    Node *node = add();
+
+    for (;;) {
+        if (consume("<")) 
+            node = new_node(ND_LT, node, add());
+        else if (consume("<="))
+            node = new_node(ND_LE, node, add());
+        else if (consume(">"))
+            node = new_node(ND_GT, node, add());
+        else if (consume(">="))
+            node = new_node(ND_GE, node, add());
+        else
+            return node;
+    }
+}
+
+Node *add() {
+    Node *node = mul();
+    
+    for (;;) {
+        if (consume("+")) 
+            node = new_node(ND_ADD, node, mul());
+        else if (consume("-"))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+Node *mul() {
+    Node *node = unary();
+
+    for (;;) {
+        if (consume("*")) 
+            node = new_node(ND_MUL, node, unary());
+        else if (consume("/")) 
+            node = new_node(ND_DIV, node, unary());
+        else
+            return node;
+    }
+}
+
+Node *unary() {
+    if (consume("+")) {
+        return primary();
+    }
+    if (consume("-")) {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
+}
+
+Node *primary() {
+    if (consume("(")) {
+        Node *node = expr();
+        expect(")");
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
